06ab1932d21312dcff790a201f0f58041d707e76
amalulfa <amalulfadhli53@gmail.com>
2026-09-02 07:04:40 +0700

    Update data.js

--- data.js
+++ data.js
@@ -8,12 +8,10 @@
 */
 
 const encryptedSentences = [
-    "VGhlIG9jZWFuIGhpZGVzIG1hbnkgbXlzdGVyaW91cyBjcmVhdHVyZXMu",
-    "U29tZSBhbmltYWxzIGNhbiBzdXJ2aXZlIGluIHRoZSBkYXJrZXN0IGRlcHRocy4=",
-    "VGhlIGRlZXAgc2VhIHJlbWFpbnMgb25lIG9mIEVhcnRoJ3MgZ3JlYXRlc3QgbXlzdGVyaWVzLg==",
-    "U2NpZW50aXN0cyBjb250aW51ZSB0byBkaXNjb3ZlciBzdHJhbmdlIGNyZWF0dXJlcyB1bmRlcndhdGVyLg==",
-    "VGhpcyBjcmVhdHVyZSBsb29rcyBsaWtlIHNvbWV0aGluZyBmcm9tIGFub3RoZXIgd29ybGQu",
-    "VGhlIGRlZXBlciB3ZSBleHBsb3JlLCB0aGUgbW9yZSBteXN0ZXJpb3VzIHRoZSBvY2VhbiBiZWNvbWVzLg==",
-    "TmF0dXJlIGhhcyBjcmVhdGVkIGNyZWF0dHVyZXMgdW5saWtlIGFueXRoaW5nIHdlIGNhbiBpbWFnaW5lLg==",
-    "U29tZSBvZiB0aGUgc3RyYW5nZXN0IGFuaW1hbHMgbGl2ZSBmYXIgYmVsb3cgdGhlIHN1cmZhY2Uu"
+    "bWVnYWxvZG9uIHNoYXJr",
+    "bWFyaWFuYSB0cmVuY2ggbXlzdGVyeQ==",
+    "ZGVlcCBzZWEgY3JlYXR1cmVz",
+    "cHJlaGlzdG9yaWMgYXBleCBwcmVkYXRvcg==",
+    "aXMgbWVnYWxvZG9uIHN0aWxsIGFsaXZl",
+    "b2NlYW4gbXlzdGVyaWVz
 ];
